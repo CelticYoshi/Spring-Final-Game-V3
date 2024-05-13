85e0a31dcf1ee3dac2650418ab7c150c9683e76b
Drake B <drakeboard719@riversideunified.org>
2024-05-13 14:16:05 -0700

    Updated scenes

--- Spring Final Game/Assets/Scripts/PlayerHealth.cs
+++ Spring Final Game/Assets/Scripts/PlayerHealth.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float health = 1;
-    
-    // Start is called before the first frame update
+    [SerializeField] private int _health = 1;
+    public TextMeshProUGUI playerHealthText;
+
     void Start()
     {
-        
+        DisplayPlayerHealth();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(int damageAmount)
     {
-        
+        _health -= damageAmount;
+        DisplayPlayerHealth();
+    }
+
+    public void DisplayPlayerHealth()
+    {
+        playerHealthText.text = "HP: " + _health.ToString();
+    }
+
+    public int GetPlayerHealth()
+    {
+        return _health;
     }
 }
