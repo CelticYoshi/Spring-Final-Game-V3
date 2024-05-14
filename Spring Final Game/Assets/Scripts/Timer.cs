c5ac06a17d6c34b4520a3673b9ee74d6a97592ba
Drake B <drakeboard719@riversideunified.org>
2024-05-13 22:33:21 -0700

    Added timer

---
+++ Spring Final Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 180f;
+    public TextMeshProUGUI timerText;
+    private bool _isTimerRunning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGameTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_isTimerRunning)
+        {
+            if(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                // display the timer amount
+                DisplayTime(timeRemaining);
+            }
+            else 
+            {
+                timeRemaining = 0;
+                _isTimerRunning = false;
+            }
+        }
+    }     
+
+    public void StartGameTimer()
+    {
+        _isTimerRunning = true;
+    }
+
+    public void EndGameTimer()
+    {
+        _isTimerRunning = false;
+    }
+
+    public float GetTimeRemaining()
+    {
+        return timeRemaining;
+    }
+
+    private void DisplayTime(float timetoDisplay)
+    {
+        timetoDisplay += 1;
+        
+        float minutes = Mathf.FloorToInt(timetoDisplay / 60);
+        float seconds = Mathf.FloorToInt(timetoDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
